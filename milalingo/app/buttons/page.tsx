09e9b49c2c4de3f30c8157d750c079b11edf7fef
mmed <mila.medic2000@gmail.com>
2025-05-28 13:54:48 +1000

    changed button styles and added more to the buttons page

--- milalingo/app/buttons/page.tsx
+++ milalingo/app/buttons/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 const ButtonsPage = () => {
     return (
         <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
-            <Button>Primary </Button>
+            <Button>Default </Button>
+            <Button>Primary</Button>
             <Button> Primary Outline</Button>
         </div>
     );
